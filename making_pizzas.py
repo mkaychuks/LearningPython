61b70918f6a0122447414e6d9da5b260465b904f
Mkaychuks <sachufusi@gmail.com>
2019-09-12 20:45:08 +0100

    module procedures

--- making_pizzas.py
+++ making_pizzas.py
@@ -1,4 +1,9 @@
-import pizza
+from pizza import make_pizza
+from pizza import greeting as ndeewo
 
-pizza.make_pizza(16, 'pepperoni')
-pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+ndeewo('Ifeanyichukwu')
+ndeewo('Ifeanyichukwu', 'Adaobi', 'Charemon', 'Mushroom')
