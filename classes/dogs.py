b74c749b05757b5c53eab0f9d46b617e4efc9d04
Mkaychuks <sachufusi@gmail.com>
2019-09-12 21:08:05 +0100

    Initial commit

---
+++ classes/dogs.py
@@ -0,0 +1,26 @@
+# working with classes
+
+
+class Dog:
+    """ A simple dog characteristics """
+
+    def __init__(self, name, age):
+        """ Initializes name and age attributes"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate the dog is sitting down"""
+        print(
+            f'{self.name} is sitting down'
+        )
+
+    def roll_over(self):
+        """Simulate a rolling over skit """
+        print(
+            f'{self.name} learnt how to rollover at {self.age} years old'
+        )
+
+my_dog = Dog('Willie', 6)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
